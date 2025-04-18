ee653194828eea5bdc1c44f0faedbbbbe5c0802d
Jiawei Ren <rjw20020718@163.com>
2025-04-18 13:48:26 -0700

    chore: update configuration

--- setup.py
+++ setup.py
@@ -12,6 +12,10 @@ setup(
     long_description_content_type='text/markdown',
     url='https://github.com/example/simworld',
     packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'simworld.config': ['*.yaml'],
+    },
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
