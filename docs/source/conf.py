1875430c03804d7fd9c7beae4ce62bb6d8498813
Jiawei Ren <rjw20020718@163.com>
2025-06-05 19:59:22 -0700

    docs: add mock

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,8 @@ extensions = [
 
 source_suffix = ['.rst', '.md']
 
-autodoc_mock_imports = ["numpy", "pandas", "unrealcv", "unreal", "sentence_transformers", "PIL", "sklearn", "torch", "transformers", "faiss", "openai", "pyqtgraph", "PyQt5", "cv2"]
+autodoc_mock_imports = ["numpy", "pandas", "unrealcv", "unreal", "sentence_transformers", "PIL", "sklearn", "torch", "transformers", "faiss", "openai", "pyqtgraph", "PyQt5", "cv2", "IPython",
+                        ""]
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
--- docs/source/resources/simworld.config.rst
+++ docs/source/resources/simworld.config.rst
@@ -1,17 +1,6 @@
 simworld.config package
 =======================
 
-Submodules
-----------
-
-simworld.config.config\_loader module
--------------------------------------
-
-.. automodule:: simworld.config.config_loader
-   :members:
-   :undoc-members:
-   :show-inheritance:
-
 Module contents
 ---------------
 
