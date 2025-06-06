525fa470922ad3f6ab66da75ce98d6a029cdf597
Jiawei Ren <rjw20020718@163.com>
2025-06-05 19:53:32 -0700

    docs: add mock

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ extensions = [
 
 source_suffix = ['.rst', '.md']
 
-autodoc_mock_imports = ["numpy", "pandas", "unrealcv", "unreal", "sentence_transformers", "PIL", "sklearn", "torch"]
+autodoc_mock_imports = ["numpy", "pandas", "unrealcv", "unreal", "sentence_transformers", "PIL", "sklearn", "torch", "transformers", "faiss", "openai"]
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
