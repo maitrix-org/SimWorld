277413261b2c52598d0ce4ed78838595c757fd02
Jiawei Ren <rjw20020718@163.com>
2025-06-05 19:45:54 -0700

    docs: add mock

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ extensions = [
 
 source_suffix = ['.rst', '.md']
 
-autodoc_mock_imports = ["numpy", "pandas", "unrealcv"]
+autodoc_mock_imports = ["numpy", "pandas", "unrealcv", "unreal", "sentence_transformers"]
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
